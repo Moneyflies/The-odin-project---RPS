f14f91abc38f6c4d846ea25113e2de4a3bfe19f5
Moneyflies <eremchuk.boris@gmail.com>
2024-02-18 19:41:40 +0300

    add new function playGame and loop for 5 rounds

--- script.js
+++ script.js
@@ -4,52 +4,61 @@ function getRandomInt (max) {
 
 function getComputerChoice () {
     let object = ["rock", "paper", "scissors"];
-    console.log(object[getRandomInt(3)]);
+    return (object[getRandomInt(3)]);
 }
 
 function playRound (playerSelection, computerSelection) {
     let playerSelectionLowCase = playerSelection.toString().toLowerCase();
-    let computerSelectionLowCase = computerSelection.toString().toLowerCase();
+    const computerSelectionLowCase = getComputerChoice().toString().toLowerCase();
     if (playerSelectionLowCase == "rock") {
         if (computerSelectionLowCase == "rock") {
-            console.log("It's bet!");
+            return ("It's bet!");
         }
         else if (computerSelectionLowCase == "paper") {
-            console.log("You Lose! Paper beats Rock")
+            return ("You Lose! Paper beats Rock");
         }
         else if (computerSelectionLowCase == "scissors") {
-            console.log("You won! Rock beats Scissors")            
+            return ("You won! Rock beats Scissors");           
         }
         else {
-            console.log("Type just rock, paper or scissors!")
+            return ("Type just rock, paper or scissors!");
         }
     }
     if (playerSelectionLowCase == "paper") {
         if (computerSelectionLowCase == "paper") {
-            console.log("It's bet");
+            return ("It's bet");
         }
         else if (computerSelectionLowCase == "rock") {
-            console.log("You won! Paper beats Rock")
+            return ("You won! Paper beats Rock");
         }
         else if (computerSelectionLowCase == "scissors") {
-            console.log("You Lose! Scissors beats Paper")
+            return ("You Lose! Scissors beats Paper");
         }
         else {
-            console.log("Type just rock, paper or scissors!")
+            return ("Type just rock, paper or scissors!");
         }
     }
     if (playerSelectionLowCase == "scissors") {
         if (computerSelectionLowCase == "rock") {
-            console.log("You Lose! Rock beats Scissors");
+            return ("You Lose! Rock beats Scissors");
         }
         else if (computerSelectionLowCase == "paper") {
-            console.log("You won! Scissors beats Paper")
+            return ("You won! Scissors beats Paper");
         }
         else if (computerSelectionLowCase == "scissors") {
-            console.log("It's bet")
+            return ("It's bet");
         }
         else {
-            console.log("Type just rock, paper or scissors!")
+            return ("Type just rock, paper or scissors!");
         }
     }
 }
+
+const playGame = () => {
+    for(let i = 1; i <= 5; i++) {
+        let playerSelection = prompt("Text your choice");
+        console.log(playRound);
+    }
+}
+
+playGame();
